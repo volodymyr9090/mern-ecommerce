24efec351622bdf4f1c6e883c789c244b22874c6
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-23 09:34:11 +0300

    JWT http-only cookie

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -19,6 +20,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
