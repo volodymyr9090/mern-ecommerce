5a1af80695b5dcfc6ecbb61942ec800c4a3ef5b4
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-23 09:53:50 +0300

    Register user and generateToken util

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  //Set JWT as an HTTP-Only cookie
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+  });
+};
+
+export default generateToken;
