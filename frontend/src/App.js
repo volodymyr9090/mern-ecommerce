349028bf35b4541cd2e4e6eda93e3ee2a1133088
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-23 10:31:18 +0300

    LoginScreen and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { Container } from 'react-bootstrap';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <h1>Welcome To MyShop</h1>
+      <ToastContainer />
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
           <h1>Welcome to MyShop</h1>
           <Outlet />
@@ -17,6 +21,6 @@ const App = () => {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
