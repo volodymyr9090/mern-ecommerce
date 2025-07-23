d4490f1f49ac23fa374a0860a0ff8bc27ed046ef
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-23 03:57:36 +0300

    Add and remove from cart

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,23 +2,10 @@ export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
-export const updateCart = (state, item) => {
-  // Check if the item is already in the cart
-  const existItem = state.cartItems.find((x) => x._id === item._id);
-
-  if (existItem) {
-    // If exists, update quantity
-    state.cartItems = state.cartItems.map((x) =>
-      x._id === existItem._id ? item : x
-    );
-  } else {
-    // If not exists, add new item to cartItems
-    state.cartItems = [...state.cartItems, item];
-  }
-
+export const updateCart = (state) => {
   // Calculate the items price
   state.itemsPrice = addDecimals(
-    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    state.cartItems.reduce((acc, item) => Number(acc) + item.price * Number(item.qty), 0)
   );
 
   // Calculate the shipping price
