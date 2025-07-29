26289d462d7ce6cd0340e176bdbac541f5882a59
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-29 13:50:03 +0300

    Fixed warning

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} />
+              <img src={logo} alt="MyShop" />
               MyShop
             </Navbar.Brand>
           </LinkContainer>
