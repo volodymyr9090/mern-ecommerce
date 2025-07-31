eb0cd67d99ca3fcf9d374cc9cdc59f639e4c2e8c
volodymyr9090 <volodymyrmalenko9@gmail.com>
2025-07-31 13:54:23 +0300

    List users for admin

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -76,6 +76,9 @@ const Header = () => {
                   <LinkContainer to="/admin/orderlist">
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
                 </NavDropdown>
               )}
             </Nav>
